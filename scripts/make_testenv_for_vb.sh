c78743a14d6c7ccf7bfb8219aadb9ed3e5435b58
Björn Nagel <kontakt@bjoernnagel.de>
2025-12-18 21:43:50 +0100

    add script for testing in virtualbox

---
+++ scripts/make_testenv_for_vb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir for_testing_get_costs
+mkdir ./for_testing_get_costs/1036\ Siebert\ und\ Partner
+mkdir ./for_testing_get_costs/1036\ Siebert\ und\ Partner/-\ 06\ Lohnabrechnung\ _\ Journal
+mkdir ./for_testing_get_costs/1036\ Siebert\ und\ Partner/-\ 06\ Lohnabrechnung\ _\ Journal/2025
+mkdir ./for_testing_get_costs/1036\ Siebert\ und\ Partner/-\ 06\ Lohnabrechnung\ _\ Journal/2025/-\ 01\ Lohnjournale
+
+mkdir ./for_testing_get_costs/2000\ Finanzverwaltung
+mkdir ./for_testing_get_costs/2000\ Finanzverwaltung/-\ 97\ Automatische\ Lohn-\ und\ Nebenkosten\ Auswertung\ \(Finanztabelle\) 
+mkdir ./for_testing_get_costs/2000\ Finanzverwaltung/-\ 97\ Automatische\ Lohn-\ und\ Nebenkosten\ Auswertung\ \(Finanztabelle\)/-\ 01\ Rohdaten\ Zur\ Auswertung
+mkdir ./for_testing_get_costs/2000\ Finanzverwaltung/-\ 97\ Automatische\ Lohn-\ und\ Nebenkosten\ Auswertung\ \(Finanztabelle\)/-\ 01\ Rohdaten\ Zur\ Auswertung/2025
+
+mkdir ./for_testing_get_costs/2000\ Finanzverwaltung/-\ 97\ Automatische\ Lohn-\ und\ Nebenkosten\ Auswertung\ \(Finanztabelle\)/-\ 02\ Ergebnisse\ der\ Auswertung\ \(Monatsweise\)

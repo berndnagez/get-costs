84b9ddf00d19ee493564ae4001ce0019a07aae82
Björn Nagel <kontakt@bjoernnagel.de>
2026-01-19 13:17:01 +0100

    Change string to float to display zero-values in resultfile

--- src/get_costs/costs_splitter.py
+++ src/get_costs/costs_splitter.py
@@ -5,7 +5,7 @@ def create_formulas_for_projects(keys_and_value_list, divisor, splitted_values_d
         if value != 'nan' and value != '0.0':
             if "bAV" in key or "Gehalt" in key or "Sozialv" in key or "Umlagen" in key:
                 if value == None:
-                    splitted_values_dict[key] = 0.00
+                    splitted_values_dict[key] = 0.0
                     print(f'Der Wert für "{key}" wurde auf 0,00 Euro gesetzt.')
                 else:
                     # hier werden die S&P-Daten für Paul mit einem negativen Vorzeichen versehen
@@ -20,14 +20,14 @@ def create_formulas_for_project(keys_and_value_list, divisor, splitted_values_di
         if value != 'nan' and value != '0.0':
             if "bAV" in key or "Gehalt" in key or "Sozialv" in key or "U1" in key or "U2" in key or "InsoU" in key:
                 if value == None:
-                    splitted_values_dict[key] = 0.00
+                    splitted_values_dict[key] = 0.0
                     print(f'Der Wert für "{key}" wurde auf 0,00 Euro gesetzt.')
                 else:
                     splitted_values_dict[key] = f'=ROUND({value}{divisor}, 2)'
             else:
                 # hier kehre ich das Vorzeichen von Pauls negativen Zahlen um
                 if value == None:
-                    splitted_values_dict[key] = 0.00
+                    splitted_values_dict[key] = 0.0
                     #print(f'Der Wert für "{key}" bei value "{value}" wurde auf 0,00 Euro gesetzt.')
                 else:
                     if float(value) < 0:
@@ -43,14 +43,14 @@ def create_formulas_for_accounting_person(project_id, keys_and_value_list, divis
         if value != 'nan' and value != '0.0':
             if "bAV" in key or "Gehalt" in key or "Sozialv" in key or "U1" in key or "U2" in key or "InsoU" in key:
                 if value == None:
-                    splitted_values_dict[key] = 0.00
+                    splitted_values_dict[key] = 0.0
                     print(f'Der Wert für "{key}" wurde auf 0,00 Euro gesetzt.')
                 else:
                     sum += float(value)
             else:
                 # hier kehre ich das Vorzeichen von Pauls negativen Zahlen um
                 if value == None:
-                    splitted_values_dict[key] = 0.00
+                    splitted_values_dict[key] = 0.0
                     #print(f'Der Wert für "{key}" wurde auf 0,00 Euro gesetzt.')
                 else:
                     if float(value) < 0:
@@ -68,7 +68,7 @@ def create_formulas_for_cleaning_person(project_id, keys_and_value_list, splitte
         if value != 'nan' and value != '0.0':
             if "bAV" in key or "Gehalt" in key or "Sozialv" in key or "U1" in key or "U2" in key or "InsoU" in key:
                 if value == None:
-                    splitted_values_dict[key] = 0.00
+                    splitted_values_dict[key] = 0.0
                     print(f'Der Wert für "{key}" wurde auf 0,00 Euro gesetzt.')
                 else:
                     sum += float(value)
@@ -86,14 +86,14 @@ def get_pay(project):
 
 def get_social_insurance(project):
     if not project.get('KV-AG-Beitrag') and not project.get('RV-AG-Beitrag') and not project.get('AV-AG-Beitrag') and not project.get('PV-AG-Beitrag'):
-        return '0.0'
+        return 0.0
     insurance = float(project.get('KV-AG-Beitrag')) + float(project.get('RV-AG-Beitrag')) + float(project.get('AV-AG-Beitrag')) + float(project.get('PV-AG-Beitrag'))
     return str(insurance)
 
 
 def get_umlagen(project):
     if not project.get('U1 - Umlage 1') and not project.get('U2 - Umlage 2') and not project.get('InsoU - Insolvenzgeldumlage'):
-        return '0.0'
+        return 0.0
     umlagen = float(project.get('U1 - Umlage 1')) + float(project.get('U2 - Umlage 2')) + float(project.get('InsoU - Insolvenzgeldumlage'))
     return str(umlagen)
 

7e8237a7f25c8d4c59f5980395794d6ac0927708
Björn Nagel <kontakt@bjoernnagel.de>
2026-01-28 16:01:51 +0100

    Delete old test

--- tests/test_file_path_creator.py
+++
@@ -1,8 +0,0 @@
-from src.get_costs import path_creator
-import pytest
-
-def test_validate_file_exists():
-    with pytest.raises(path_creator.PathNotFoundError):
-        path_creator.validate_path_exists(["./data/24_01_24 Lohnjournal Januar 2024.xlsx", "./data/24_02_24 Lohnjournal Januar 2024.xlsx"])
-
-    path_creator.validate_path_exists(["main.py"])

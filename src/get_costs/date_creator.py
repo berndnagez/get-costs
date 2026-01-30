cdbc6ce8cfb7708e0bc63eb37a4cc0fbf62c7a8b
Björn Nagel <kontakt@bjoernnagel.de>
2026-01-30 10:56:02 +0100

    Clean up date_creator

--- src/get_costs/date_creator.py
+++ src/get_costs/date_creator.py
@@ -4,31 +4,31 @@ import re
 def validate(journal_name):
     pattern = re.compile(r"\d{2}_\d{2}.*")
     if not re.fullmatch(pattern, journal_name):
-        print(f"Die Datei '{journal_name}' hat keinen validen Dateinamen. Der Dateiname muss mit dem Jahr, gefolgt vom Monat anfangen.")
-        print(f"Bspw. 24_04 für das Lohnjournal des Aprils 2024.")
+        print(f"Die Datei '{journal_name}' hat keinen validen Dateinamen. Der Dateiname muss mit dem Jahr, gefolgt von Monat und Tag anfangen.")
+        print(f"Bspw. 24_04_23 für das Lohnjournal des Aprils 2024.")
         exit('Programmabbruch.')
 
-def split(filename):
+def split_year_month(filename):
     name_parts = filename.split("_")
     year = name_parts[0]
     month = name_parts[1]
     return year, month
 
 def get_sheet_name(filename):
-    year, month = split(filename)
+    year, month = split_year_month(filename)
+    print(split_year_month(filename))
+    print(f'Jahr: {year}, Monat: {month}')
     sheet_name = f'{year}_{month}'
     return sheet_name
 
 def get_date_from(filename):
-    year, month = split(filename)
+    year, month = split_year_month(filename)
     date_str = f'01-{month}-{year}'
     date_object = datetime.strptime(date_str, '%d-%m-%y').date()
     return date_object
 
 def get_year(filename):
     return filename.split("_")[0]
-
-
 
 def get_date_object(sheet_name):
     parts_of_sheet_name = sheet_name.split("_")
@@ -37,19 +37,3 @@ def get_date_object(sheet_name):
     date_str = f'01-{month}-{year}'
     date_object = datetime.strptime(date_str, '%d-%m-%y').date()
     return date_object
-
-
-def show_debug_infos():
-    print(get_year("23_12_20 Lohnjournal Dezember 2023.xlsx"))
-    sheet_name = get_sheet_name("23_12_20 Lohnjournal Dezember 2023.xlsx")
-    date = get_date_from("23_12_20 Lohnjournal Dezember 2023.xlsx")
-    print(sheet_name)
-    print(date)
-    validate("23_12_20 Lohnjournal Dezember 2023.xlsx")
-    validate("Lohnjournal Dezember 2023.xlsx")
-
-
-if __name__ == "__main__":
-    debug = True
-    if (debug):
-        show_debug_infos()
